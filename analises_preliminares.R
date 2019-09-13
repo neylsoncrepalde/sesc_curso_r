9237f92519479e52227eb61732cf0acf09846ece
Neylson Crepalde <neylsoncrepalde@gmail.com>
2019-09-13 16:03:03 -0300

    Update analises_preliminares.R

--- analises_preliminares.R
+++ analises_preliminares.R
@@ -28,16 +28,37 @@ freq(bd$nomeinst)
 # Sexo do respondente
 freq(bd$sexo)
 
+## Para visualizar com eficiência a informação
+## do sexo, vamos utilizar um gráfico de barras
+barplot(table(bd$sexo))
+
 # Gráfico do sexo do respondente
 # Sem labels
 ggplot(bd, aes(sexo)) +
   geom_bar()
 
-# Com labels, precisamos de um pequeno pré-processamento
+
+## Gráfico de barras com ggplot mais elaborado
+bd %>% 
+  ggplot(aes(x=sexo,fill=sexo)) +
+  geom_bar() +
+  theme_bw() +
+  labs(x="Sexo", y="Frequência",
+       title = "Sexo",
+       subtitle = "Distribuição",
+       caption = "Elaboração do autor") +
+  scale_fill_manual(values = c("purple","orange"))
+
+######################################
+## colocando o rótulo
+## Para colocar o rótulo, precisamos de um pequeno pré-processamento
 bd %>% 
   group_by(sexo) %>% 
-  summarise(n = n()) %>% 
-  ggplot(aes(x=sexo, y=n)) +
+  summarise(n = n(),
+            med = mean(nrem)) %>% 
+  ggplot(aes(x=sexo, y=n, label=n,
+             fill = sexo)) +
   geom_col() +
-  geom_text(aes(label = n), vjust = -.5)
-
+  geom_text(hjust = -0.2) +
+  ylim(0,30) +
+  coord_flip()
